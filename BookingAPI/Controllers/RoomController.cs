f76df3068aff07adb74027e69dff9e531221421e
trongan123 <62749635+trongan123@users.noreply.github.com>
2022-06-06 12:57:14 +0700

    ASM_Booking

---
+++ BookingAPI/Controllers/RoomController.cs
@@ -0,0 +1,26 @@
+﻿using DataAccess.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Repositories.IService;
+
+namespace BookingAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class RoomController : ControllerBase
+    {
+        private readonly IRoomService accountService;
+
+        public RoomController(IRoomService account)
+        {
+            this.accountService = account;
+        }
+
+        [HttpGet]
+        public ActionResult<IEnumerable<Room>> GetAccount() => accountService.GetRooms();
+
+
+
+
+    }
+}
